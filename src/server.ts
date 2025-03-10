27f93fe1dde62800cd31e062407835e02fff47c0
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-03-10 17:21:47 +0600

    removed node_modules

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 // import mongoose from 'mongoose'
 import app from './app'
 import { Server } from 'http'
+import config from './config'
+import mongoose from 'mongoose'
 
 let server: Server
 
 async function main() {
   try {
-    // console.log(config)
-    // await mongoose.connect(config.dataBase_url as string)
-    server = app.listen(5050, () => {
-      console.log(` surver running on port 5050`)
+    console.log(config)
+    await mongoose.connect(config.dataBase_url as string)
+    server = app.listen(config.port, () => {
+      console.log(` surver running on port ${config.port}`)
     })
   } catch (error) {
     console.log(error)
