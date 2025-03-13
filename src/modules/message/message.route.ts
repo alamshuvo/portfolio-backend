8fac053641b96af07a7daecbedb6c7a73c59eb5d
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-03-13 22:56:29 +0600

    some code added

--- src/modules/message/message.route.ts
+++ src/modules/message/message.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import validateRequest from "../../middleware/validateRequest";
 import { messageValidation } from "./message.validation";
+import { messageController } from "./message.controller";
 
 
 
 const messageRoute = Router();
-messageRoute.post('/',validateRequest(messageValidation.messageValidationSchema))
-messageRoute.get('/',)
+messageRoute.post('/',validateRequest(messageValidation.messageValidationSchema),messageController.createMessage)
+messageRoute.get('/',messageController.getAllMessage)
 
 
 export default messageRoute
